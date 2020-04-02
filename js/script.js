358f3089ce813b8d1fe4ed62af0c9dfa684bb6fb
Gabriela Guida <gabrielaguida7@gmail.com>
2020-04-01 23:00:40 -0300

    WEB-21

--- js/script.js
+++ js/script.js
@@ -4876,8 +4876,18 @@ var periodic_table = {
 // this function will be use to show the element properties inside the function displayElement()
 function writeOnHTMLElements(elementList, text){
     for (index = 0; index < elementList.length; index++){
-        elementList[index].innerText = text;
+        console.log(typeof text);
+        if (typeof text == "string" || typeof text == "number"){
+            elementList[index].innerHTML = text;
+        }else if (typeof text == "object" && Array.isArray(text)) {
+            let topics = "";
+            text.forEach(arrElement => {
+                topics += `<li>${arrElement}</li>`;
+            })
+            elementList[index].innerHTML = topics;
+        }
     }
+
 }
 
 // function that returns with element symbol
@@ -4907,24 +4917,51 @@ function clickCallback(event){
 function displayElement(element, elementClasses){
     var number = document.getElementsByClassName("element-number");
     writeOnHTMLElements(number, element.number);
+
     var symbol = document.getElementsByClassName("element-symbol");
     writeOnHTMLElements(symbol, element.symbol);
+    
     var name = document.getElementsByClassName("element-name");
     writeOnHTMLElements(name, element.name);
+    
     var weight = document.getElementsByClassName("element-weight");
     writeOnHTMLElements(weight, element.atomic_mass);
+    
     var summary = document.getElementsByClassName("element-summary");
     writeOnHTMLElements(summary, element.summary);
+    
     var phase = document.getElementsByClassName("element-phase");
     writeOnHTMLElements(phase, element.phase);
+    
     var category = document.getElementsByClassName("element-category");
     writeOnHTMLElements(category, element.category);
+    
     var boil = document.getElementsByClassName("element-boil");
     writeOnHTMLElements(boil, element.boil);
+    
     var melt = document.getElementsByClassName("element-melt");
     writeOnHTMLElements(melt, element.melt);
+    
     var density = document.getElementsByClassName("element-density");
     writeOnHTMLElements(density, element.density);
+    
+    var configuration = document.getElementsByClassName("element-configuration");
+    // using regular expression (regex or regexp) to replace the electron configuration string
+    var configurationReplaced = element.electron_configuration.replace(/(\d[spdf])(\d+)/g, "$1<sup>$2</sup>")
+    writeOnHTMLElements(configuration, configurationReplaced);
+    
+    var affinity = document.getElementsByClassName("element-affinity");
+    writeOnHTMLElements(affinity, element.electron_affinity);
+    
+    var electronegativity = document.getElementsByClassName("element-electronegativity");
+    writeOnHTMLElements(electronegativity, element.electronegativity_pauling);
+    
+    var ionization = document.getElementsByClassName("element-ionization");
+    var ionizationEnergies = element.ionization_energies.map(arrEl => {
+        return arrEl + " kJ/mol";
+    });
+    writeOnHTMLElements(ionization, ionizationEnergies);
+    
     document.getElementById("element_wrapper").className = "";
     document.getElementById("element_box").className = elementClasses;
 }
